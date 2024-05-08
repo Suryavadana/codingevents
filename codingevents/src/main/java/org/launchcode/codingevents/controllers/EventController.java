1bbed94661d7ce8bff5c20794d9039cc2599d4ee
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-05-07 21:58:45 -0500

    add Unique id

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,23 +15,11 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<Event> events = new ArrayList<>();
- //   public String getEVents(Model model){
-//        // Create an empty list to store event names
-//        List<String> events = new ArrayList<>();
-//
-//        // Add some event names to the list
-//        events.add("Code With Pride");
-//        events.add("Strange Loop");
-//        events.add("Apple WWDC");
-//        events.add("SpringOne platform");
-//
-//        // Add the list of event names to the model
-//        model.addAttribute("events", events);
+
     @GetMapping
     public String displayAllEvents(Model model){
         // Add the list of events to the model
-        model.addAttribute("events",events);
+        model.addAttribute("events", EventData.getAll());
         // Return the name of the template to render
         return "events/index";// Assuming you have a template named 'index.html' in the 'events' directory
     }
@@ -42,7 +31,7 @@ public class EventController {
     //lives at /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName , @RequestParam String eventDescription){
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:/events";
     }
 }
