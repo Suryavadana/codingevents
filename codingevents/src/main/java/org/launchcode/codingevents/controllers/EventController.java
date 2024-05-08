3f0ae17611dd44ce178e88f8a04504bae1bc6afa
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-05-07 22:45:47 -0500

    Delete

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -18,20 +18,47 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model){
+        model.addAttribute("title","All Events");
         // Add the list of events to the model
         model.addAttribute("events", EventData.getAll());
         // Return the name of the template to render
         return "events/index";// Assuming you have a template named 'index.html' in the 'events' directory
     }
     //lives at /events/create
+//    @GetMapping("create")
+//    public String renderCreateEventForm(){
+//        return "events/create";
+//    }
+
+
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
     //lives at /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName , @RequestParam String eventDescription){
         EventData.add(new Event(eventName, eventDescription));
+        return "redirect:/events";
+    }
+    // lives at "/events/delete"
+    @GetMapping("delete")
+    public String displayDeleteForm(Model model){
+        model.addAttribute("title","Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam (required = false) int[]eventIds){
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
         return "redirect:/events";
     }
 }
