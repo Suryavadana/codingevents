9b8902788a8213279678a45b458d0b7cdd38cb03
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-05-26 20:10:45 -0500

    persistent-model

--- codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
+++ codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,12 +1,17 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
-
 import java.util.Objects;
 
+@Entity
 public class Event {
+
+    @Id
+    @GeneratedValue
     private int id;
-    private static int nextId = 1;
     @NotBlank(message = "Name is required.")
     @Size (min = 3, max = 50 , message = "Name must be between 3 and 50 characters.")
     private String name;
@@ -24,7 +29,6 @@ public class Event {
     @Positive(message="Number of attendees must be one or more.")
     private int numberOfAttendees;
     public Event(String name, String description , String contactEmail ,String location, boolean registrationRequired, int numberOfAttendees, EventType type) {
-        this();
         this.name = name;
         this.description=description;
         this.contactEmail=contactEmail;
@@ -34,10 +38,7 @@ public class Event {
         this.numberOfAttendees=numberOfAttendees;
 
     }
-    public Event(){
-        this.id=nextId;
-        nextId++;
-    }
+    public Event(){}
     public String getName() {
         return name;
     }
